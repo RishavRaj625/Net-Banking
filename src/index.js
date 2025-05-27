a7a6bee657332ffdcac82b26b90bd9a52f481e36
Rishav Raj <165421390+RishavRaj625@users.noreply.github.com>
2025-05-28 02:59:39 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'; // Change this line
 import App from './App';
+import './styles/App.css'; // Import global styles
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
